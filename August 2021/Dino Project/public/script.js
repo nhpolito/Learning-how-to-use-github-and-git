cede6933e0046340e874ea5779f44ea1c31e8af9
Nick Polito <nhpolito@gmail.com>
2023-04-17 17:29:52 -0700

    remove deleted files

--- August 2021/Dino Project/public/script.js
+++
@@ -1,8 +0,0 @@
-console.log('script.js loaded')
-
-getDinoName()
-async function getDinoName() {
-    const response = await fetch('/dinoname');
-    const data = await response.json();
-    console.log(data)
-}
